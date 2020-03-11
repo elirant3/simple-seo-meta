91f0b1d8fef1365c7137ecd66b0d7618dede2c2e
kali <elirant3@gmail.com>
2020-03-11 02:39:59 +0200

    empty

---
+++ simple-seo-meta.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Simple Seo Metadata
+Plugin URI: https://github.com/elirant3/simple-seo-meta
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: Eliran Biton
+Author URI: https://github.com/elirant3
+License: A "Slug" license name e.g. GPL2
+*/
